55b2d9507a23d02b945824f937adda6eeec1494a
Niko Stenberg <niko.stenberg@protonmail.com>
2026-03-03 14:13:28 +0200

    Fix: deployment issues

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,14 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { preprocess: vitePreprocess(), kit: { adapter: adapter() } };
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/stenberg-n.github.io'
+    }
+  }
+};
 
 export default config;
