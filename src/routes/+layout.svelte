03c2db5f5fa070d04d429186d74310504154fa8c
Niko Stenberg <niko.stenberg@protonmail.com>
2026-03-03 13:35:45 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav style="background-color: rgba(15,15,15,0.8); padding: 1rem;">
+  <a href="/">Home</a>
+  <a href="/projects">Projects</a>
+</nav>
+
+{@render children()}
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<h1>All projects</h1>
+
+<a href="/projects/finance-tracker">Finance Tracker</a>
+<a href="/projects/focusboard">FocusBoard</a>
+<a href="/projects/waste-classifier">Waste Classifier</a>
